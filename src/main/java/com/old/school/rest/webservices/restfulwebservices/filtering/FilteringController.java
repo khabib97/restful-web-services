befb6b5ad2abad33b8841b875f246bba6b1af2ff
khabib97 <kawser.habib.dev@gmail.com>
2021-03-15 11:45:59 +0600

    static filtering approach 2

--- src/main/java/com/old/school/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/old/school/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -11,11 +11,11 @@ public class FilteringController {
 
 	@GetMapping("/filtered-user")
 	public User getUser() {
-		return new User("khabib97","1013ssew");
+		return new User("khabib97","1013ssew", "dffe");
 	}
 	
 	@GetMapping("/filtered-user-list")
 	public List<User> getAllUser() {
-		return Arrays.asList(new User("khabib97","1013ssew"),new User("sonet","errsew")) ;
+		return Arrays.asList(new User("khabib97","1013ssew", "dffwe"),new User("sonet","errsew","tpp")) ;
 	}
 }
--- src/main/java/com/old/school/rest/webservices/restfulwebservices/filtering/User.java
+++ src/main/java/com/old/school/rest/webservices/restfulwebservices/filtering/User.java
@@ -1,17 +1,23 @@
 package com.old.school.rest.webservices.restfulwebservices.filtering;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+//approach 2: static filtering
+@JsonIgnoreProperties(value = {"token"})
 public class User {
 	
 	private String username;
-	@JsonIgnore
+	@JsonIgnore // approach 1: static filtering {best one}
 	private String password;
 	
-	public User(String username, String password) {
+	private String token;
+
+	public User(String username, String password, String token) {
 		super();
 		this.username = username;
 		this.password = password;
+		this.token = token;
 	}
 
 	public String getUsername() {
@@ -28,6 +34,15 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
 	}
 
 }
